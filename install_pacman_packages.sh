28d6e15d0322505f3de6173977c9a6f1107d73c9
Florian Kempenich <Flori@nKempenich.com>
2022-10-21 15:45:05 +0100

    Improve initialize script

---
+++ install_pacman_packages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd "$DIR"
+
+sudo pacman -S --noconfirm xorg-xinput
+sudo pacman -S --noconfirm figlet
+sudo pacman -S --noconfirm xorg-xinput
